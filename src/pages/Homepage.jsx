8eeb2ef6b0605ebbee8ba0b89b06ac2dc7ed00e3
DragunDev <plarisanmarc@gmail.com>
2025-06-10 20:54:54 +0800

    Update homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,7 +6,7 @@ function Homepage() {
     <div className={styles.wrapper}>
       <Card
         name="Marc Plarisan"
-        description="Greetings, I'm an aspiring software developer, avid reader of books, daily journal writer"
+        description="I’m an incoming 4th-year IT student specializing in mobile development for both iOS and Android, as well as backend development. Furthermore, I also have experience in web development and game development, allowing me to build versatile and engaging digital solutions across multiple platforms."
         socialLinks={{
           github: "https://github.com/DragunWF",
           linkedin: "https://linkedin.com/in/marcplarisan",
