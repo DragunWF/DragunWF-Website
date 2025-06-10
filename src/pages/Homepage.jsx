3495ff64ebfed357dd65be3a0d8f6a74038d59dc
DragunDev <plarisanmarc@gmail.com>
2025-06-10 20:26:31 +0800

    Create base card component in the homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,21 @@
+import styles from "./Homepage.module.css";
+import Card from "../components/Card";
+
 function Homepage() {
-  return <div>Marc Plarisan</div>;
+  return (
+    <div className={styles.wrapper}>
+      <Card
+        name="Marc Plarisan"
+        description="Greetings, I'm an aspiring software developer, avid reader of books, daily journal writer"
+        socialLinks={{
+          github: "https://github.com/DragunWF",
+          linkedin: "https://linkedin.com/in/marcplarisan",
+          twitter: "https://twitter.com/dragunwf",
+          email: "dragunwf@example.com",
+        }}
+      />
+    </div>
+  );
 }
 
 export default Homepage;
