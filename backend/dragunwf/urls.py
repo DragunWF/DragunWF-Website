a5cc549c742c0372962107c4e3e88fcd75e33292
DragunDev <plarisanmarc@gmail.com>
2025-06-13 02:09:08 +0800

    Update urls.py - Implement auto redirection to admin site when root path is visited

--- backend/dragunwf/urls.py
+++ backend/dragunwf/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
     path('admin/', admin.site.urls),
 ]
