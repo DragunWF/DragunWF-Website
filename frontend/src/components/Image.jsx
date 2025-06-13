99c292230cab9889e5cdf68a1d3c3b233e7777a6
DragunDev <plarisanmarc@gmail.com>
2025-06-13 20:26:21 +0800

    Update Image.jsx

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -3,7 +3,8 @@ import styles from "./Image.module.css";
 
 function Image({ src }) {
   const pathNodes = src.split("/");
-  const altName = pathNodes[pathNodes.length - 1];
+  let altName =
+    pathNodes.length > 0 ? pathNodes[pathNodes.length - 1] : "Image";
   return <img className={styles.dynamicImg} src={src} alt={altName}></img>;
 }
 
