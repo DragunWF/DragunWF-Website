42742531dc8d0469a34d6f167ad59025c401ada7
DragunDev <plarisanmarc@gmail.com>
2025-06-13 20:51:15 +0800

    Implement image link field to blog post

--- backend/interactions/models.py
+++ backend/interactions/models.py
@@ -4,6 +4,12 @@ from djongo import models
 class BlogPost(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=128)
+    image_link = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        default=""
+    )
     description = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
