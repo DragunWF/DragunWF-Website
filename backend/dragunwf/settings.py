c78327dd54ee61800ac34bdd21b2372d44e4c00f
DragunDev <plarisanmarc@gmail.com>
2025-06-13 17:04:55 +0800

    Update settings.py - modify corsheaders

--- backend/dragunwf/settings.py
+++ backend/dragunwf/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ["dashboard-dragunwf.onrender.com", "localhost", "127.0.0.1"]
 # CORS Settings
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://localhost:3000",
     "https://dragunwf.vercel.app",
 ]
 
@@ -63,6 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'interactions'
 ]
@@ -70,6 +72,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
