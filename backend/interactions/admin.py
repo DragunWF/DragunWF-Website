4dc8f774064c33aa67d928e6460230d080f67f2e
DragunDev <plarisanmarc@gmail.com>
2025-06-13 15:23:22 +0800

    Create BlogPost and Message data models

--- backend/interactions/admin.py
+++ backend/interactions/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import BlogPost, Message
 
-# Register your models here.
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ["title", "date_created", "date_updated"]
+    search_fields = ["title"]
+    list_per_page = 10
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ["content", "date_sent"]
+
+
+# Registration of admin models
+admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(Message, MessageAdmin)
--- backend/interactions/models.py
+++ backend/interactions/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.TextField(max_length=2500)
+    date_sent = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message: {self.content[:50]}..."
