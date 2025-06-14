046742c9b38412b96333281068a7d3243a8b3259
DragunDev <plarisanmarc@gmail.com>
2025-06-15 01:29:47 +0800

    Improve Apology Page

--- frontend/src/pages/ApologyPage.jsx
+++ frontend/src/pages/ApologyPage.jsx
@@ -1,5 +1,18 @@
+import styles from "./ApologyPage.module.css";
+import Card from "../components/Card";
+import Title from "../components/Title";
+import Image from "../components/Image";
+
 function ApologyPage() {
-  return <div>Page Not Found</div>;
+  return (
+    <div className={styles.wrapper}>
+      {/* Yes, the max width. It may not be a blog, but a bigger card is preferred */}
+      <Card maxWidthType="blog">
+        <Title>Page Not Found!</Title>
+        <Image src="/page-not-found.webp" />
+      </Card>
+    </div>
+  );
 }
 
 export default ApologyPage;
