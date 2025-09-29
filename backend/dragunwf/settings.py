16dc6b68fe792bb3644a6977af91da1dfca5edd3
DragunWF <plarisanmarc2@gmail.com>
2025-09-30 01:01:51 +0800

    refactor(backend): create dynamic debug mode based on the Render environment variable

--- backend/dragunwf/settings.py
+++ backend/dragunwf/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('RENDER') != 'true'
 
 ALLOWED_HOSTS = ["dashboard-dragunwf.onrender.com", "localhost", "127.0.0.1"]
 
